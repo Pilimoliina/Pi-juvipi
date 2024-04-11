43ac069096fa85e4624029fd47fd5bf490fecc43
Pilimoliina <pmolina@udesa.edu.ar>
2024-04-11 11:44:58 -0300

    Arregle un par de detalles

---
+++ myapp/controllers/indexController.js
@@ -0,0 +1,11 @@
+let product = require('../db/product');
+
+let indexC = {
+    register : function (req, res) {
+        res.render('register');
+    }
+
+
+}
+
+module.exports = indexC;
--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var indexC = require('../controllers/indexController');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/register', indexC.register)
 
 
 
