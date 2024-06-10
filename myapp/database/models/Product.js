59da96f29a570a2290e8628bba9918c4af6daba3
Pilimoliina <pmolina@udesa.edu.ar>
2024-06-10 11:10:24 -0300

    arregle detalles en el model de product

--- myapp/database/models/Product.js
+++ myapp/database/models/Product.js
@@ -5,14 +5,33 @@ module.exports = function (sequelize, DataTypes) {
 
     /* Configuracion de las columnas */
     let cols = {
-        id: {
-            autoIncrement : true,
-            primaryKey : true,
-            type : DataTypes.INTEGER
+        id:{
+            autoIncrement:true,
+            primaryKey: true,
+            type: DataTypes.INTEGER
         },
-        name: {
-            type : DataTypes.STRING
+        idUsuario:{
+            type: DataTypes.STRING
+        },
+        fotoproducto:{
+            type: DataTypes.STRING
+        },
+        nombreProducto:{
+            type: DataTypes.STRING
+        },
+        descripcionProducto:{
+            type: DataTypes.STRING
+        },
+        createdAt:{
+            type: DataTypes.DATE
+        },
+        updatedAt:{
+            type: DataTypes.DATE
+        },
+        deletedAt:{
+            type: DataTypes.DATE
         }
+
   	}
         /* Config de la tabla */
         let config = {
@@ -23,15 +42,14 @@ module.exports = function (sequelize, DataTypes) {
     
         let Producto = sequelize.define(alias, cols, config);
         Producto.associate = function(models) {
-            Producto.belongsTo(models.usuarios, {
+            Producto.belongsTo(models.Usuario, {
                 as: "usuario",
                 foreignKey: "usuarios_id"
             });
     
-            Producto.belongsToMany( models.comentarios, {
+            Producto.hasMany(models.Comentario, {
                  as: "comentarios",
-            
-                 timestamps: false
+                 foreignKey: "productos_id"
              });
         }
         
