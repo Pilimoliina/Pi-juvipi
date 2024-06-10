8a29c112407164f07d3cf6da032f69c3b093b1e0
Pilimoliina <pmolina@udesa.edu.ar>
2024-06-10 09:57:55 -0300

    corregi el modelo producto

--- myapp/database/models/Product.js
+++ myapp/database/models/Product.js
@@ -1,28 +1,42 @@
 module.exports = function (sequelize, DataTypes) {
 
     /* alias */
-    let alias = "Product";
+    let alias = "Producto";
 
     /* Configuracion de las columnas */
     let cols = {
         id: {
             autoIncrement : true,
             primaryKey : true,
-            type : dataTypes.INTEGER
+            type : DataTypes.INTEGER
         },
         name: {
-            type : dataTypes.STRING
+            type : DataTypes.STRING
         }
   	}
+        /* Config de la tabla */
+        let config = {
+            tableName: "productos",
+            timestamps: false,
+            underscored: true
+        }
+    
+        let Producto = sequelize.define(alias, cols, config);
+        Producto.associate = function(models) {
+            Producto.belongsTo(models.usuarios, {
+                as: "usuario",
+                foreignKey: "usuarios_id"
+            });
+    
+            Producto.belongsToMany( models.comentarios, {
+                 as: "comentarios",
+            
+                 timestamps: false
+             });
+        }
+        
+   
+
+        return Producto;
     
 }
-    /* Config de la tabla */
-    let config = {
-        tableName: "product",
-        timestamps: false,
-        underscored: true
-    }
-
-    let Product = sequelize.define(alias, cols, config);
-    return Product;
-    
